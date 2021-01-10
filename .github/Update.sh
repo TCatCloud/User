17321d1d1441c951de7cb03aff830573442945bc
TCatCloud <runner@fv-az68-408.1lfcxu4ptdauvfocq3dhkrmrwb.gx.internal.cloudapp.net>
2021-01-10 12:55:42 +0000

    Update

---
+++ .github/Update.sh
@@ -0,0 +1,2 @@
+sed -i 's?./theme/?https://cdn.jsdelivr.net/gh/TCatCloud/User@master/theme/?g' umi.js
+sed -i 's?url(./static/?url(https://cdn.jsdelivr.net/gh/TCatCloud/User@master/static/?g' umi.css
